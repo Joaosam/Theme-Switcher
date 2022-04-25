2b050a4cfa39aeaab4af1994a9987af85f97a84c
João Pedro Mendes dos Santos <66846607+Joaosam@users.noreply.github.com>
2022-04-25 17:01:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// Toda vez que a página for carregada essa função será chamada
+window.onload = function verifyTheme() {
+  // Verifico qual o tema existente no Local Storage
+  const theme = localStorage.getItem('Theme')
+  console.log(theme)
+  if (theme == null) {
+    localStorage.setItem('Theme', 'light')
+    // Invoco a classe dark, marcando meu checkbox como true
+  } else if (theme == 'dark') {
+    document.body.classList.add('dark')
+    document.getElementById('checkTheme').checked = true
+  }
+}
+
+// Verifico se a classe dark está habilitada ou não, com isso, defino o valor do Local Storage
 function dark() {
-  document.body.classList.toggle('darks')
+  const verifyTheme = document.body.classList.toggle('dark')
+  if (verifyTheme == true) {
+    localStorage.setItem('Theme', 'dark')
+  } else {
+    localStorage.setItem('Theme', 'light')
+  }
 }
